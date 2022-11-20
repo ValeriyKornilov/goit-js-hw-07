3da5d9d26828ce6ed70b249b8660a904fbc8d88a
wellkornilov <kornilovaleriy@gmail.com>
2022-11-20 16:10:08 +0200

    20.11.2022

---
+++ js/01-gallery.js
@@ -0,0 +1,79 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryMarkup(images) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+const galleryItemsMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.innerHTML = galleryItemsMarkup;
+
+// galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+// function onGalleryItemClick(e) {
+//     e.preventDefault();
+
+//     if (!e.target.classList.contains('gallery__image')){
+//         return
+//     }
+
+//     const currentActiveImageLink = e.target.dataset.source;
+
+//     const instance = basicLightbox.create(`<img src="${currentActiveImageLink}" width="800" height="600">`);
+//     instance.show();
+
+//     galleryContainer.addEventListener('keydown', (e) => {
+//         if (e.code === "Escape") {
+//             instance.close();
+//         };
+//     });
+// };
+
+let instance;
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  galleryContainer.addEventListener("keydown", onEscPress);
+
+  const currentActiveImageLink = e.target.dataset.source;
+
+  instance = basicLightbox.create(
+    `<img src="${currentActiveImageLink}" width="800" height="600">`,
+    {
+      onClose: (instance) => {
+        galleryContainer.removeEventListener("keydown", onEscPress);
+      },
+    }
+  );
+  instance.show();
+}
+
+function onEscPress(e) {
+  if (e.code === "Escape") {
+    instance.close();
+  }
+}
